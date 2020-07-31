57fc440c298197cd46ef627be92f773f965e4438
Jorge Ramirez <ramirez.jorgea@outlook.com>
2020-07-30 23:41:19 -0700

    Add TouchKeypad project (Need to add Mpr121 package when released)

---
+++ source/Hackster/TouchKeypad/MeadowApp.cs
@@ -0,0 +1,74 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Displays.Tft;
+using Meadow.Foundation.Graphics;
+//using Meadow.Foundation.Hid;
+using Meadow.Foundation.Leds;
+using Meadow.Hardware;
+
+namespace TouchKeypad
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        //Mpr121 sensor;
+        St7789 display;
+        RgbPwmLed onboardLed;
+        GraphicsLibrary graphics;
+
+        public MeadowApp()
+        {
+            onboardLed = new RgbPwmLed(Device,
+                 Device.Pins.OnboardLedRed,
+                 Device.Pins.OnboardLedGreen,
+                 Device.Pins.OnboardLedBlue,
+                 3.3f, 3.3f, 3.3f,
+                 Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
+            onboardLed.SetColor(Color.Red);
+
+            var config = new SpiClockConfiguration(6000, SpiClockConfiguration.Mode.Mode3);
+            var spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config);
+            display = new St7789(device: Device, spiBus: spiBus,
+                chipSelectPin: Device.Pins.D02,
+                dcPin: Device.Pins.D01,
+                resetPin: Device.Pins.D00,
+                width: 240, height: 240);
+
+            graphics = new GraphicsLibrary(display);
+            graphics.Rotation = GraphicsLibrary.RotationType._180Degrees;
+            graphics.CurrentFont = new Font12x16();
+
+            //sensor = new Mpr121(Device.CreateI2cBus(I2cBusSpeed.Standard), 90, 100);
+            //sensor.ChannelStatusesChanged += SensorChannelStatusesChanged;
+
+            onboardLed.SetColor(Color.Green);
+        }
+
+        //void SensorChannelStatusesChanged(object sender, ChannelStatusChangedEventArgs e)
+        //{
+        //    graphics.Clear();
+        //    graphics.Stroke = 1;
+
+        //    for (int i = 0; i < e.ChannelStatus.Count; i++)
+        //    {
+        //        int numpadIndex = 0;
+        //        for (int columns = 0; columns < 3; columns++)
+        //        {
+        //            for (int rows = 3; rows >= 0; rows--)
+        //            {
+        //                if (numpadIndex == i)
+        //                {
+        //                    if (e.ChannelStatus[(Mpr121.Channels)i])
+        //                        graphics.DrawRectangle(columns * 57 + 38, rows * 57 + 10, 51, 51, Meadow.Foundation.Color.Cyan, true);
+        //                    else
+        //                        graphics.DrawRectangle(columns * 57 + 38, rows * 57 + 10, 51, 51, true);
+        //                }
+        //                numpadIndex++;
+        //            }
+        //        }
+        //    }
+
+        //    graphics.Show();
+        //}
+    }
+}
