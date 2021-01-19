e3d11fcdaac0e15950f65505d5a500224bd07807
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-01-18 23:11:15 -0800

    Code cleanup for WifiClock

--- source/Hackster/WifiClock/MeadowApp.cs
+++ source/Hackster/WifiClock/MeadowApp.cs
@@ -4,7 +4,10 @@ using Meadow.Foundation;
 using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Foundation.Sensors.Temperature;
 using Meadow.Gateway.WiFi;
+using Meadow.Hardware;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,69 +15,122 @@ using WifiClock.Services;
 
 namespace WifiClock
 {
-    public class MeadowApp : App<F7Micro, MeadowApp>
+public class MeadowApp : App<F7Micro, MeadowApp>
+{
+    PushButton pushButton;
+    Max7219 display;
+    GraphicsLibrary graphics;
+    AnalogTemperature analogTemperature;
+
+    bool showDate;
+
+    public MeadowApp()
     {
-        RgbPwmLed onboardLed;
-        Max7219 display;
-        GraphicsLibrary graphics;
+        Initialize();
 
-        public MeadowApp()
-        {
-            Initialize();
-
-            Start();
-        }
-
-        void Initialize()
-        {
-            Console.WriteLine("Initialize hardware...");
-
-            onboardLed = new RgbPwmLed(device: Device,
-                redPwmPin: Device.Pins.OnboardLedRed,
-                greenPwmPin: Device.Pins.OnboardLedGreen,
-                bluePwmPin: Device.Pins.OnboardLedBlue,
-                3.3f, 3.3f, 3.3f,
-                Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
-
-            onboardLed.StartPulse(Color.Red);
-
-            display = new Max7219(Device, Device.CreateSpiBus(), Device.Pins.D01, 4, Max7219.Max7219Type.Display);
-            graphics = new GraphicsLibrary(display);
-            graphics.CurrentFont = new Font4x8();
-            graphics.Rotation = GraphicsLibrary.RotationType._180Degrees;
-
-            Device.InitWiFiAdapter().Wait();
-
-            onboardLed.StartPulse(Color.Blue);
-
-            var result = Device.WiFiAdapter.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD);
-            if (result.ConnectionStatus != ConnectionStatus.Success)
-            {
-                onboardLed.StartPulse(Color.Magenta);
-                throw new Exception($"Cannot connect to network: {result.ConnectionStatus}");
-            }
-
-            onboardLed.StartPulse(Color.Green);
-        }
-
-        async Task Start() 
-        {
-            var dateTime = await WeatherService.GetTimeAsync();
-
-            Device.SetClock(new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second));
-
-            while (true)
-            {
-                DateTime clock = DateTime.Now;
-                graphics.Clear();
-                graphics.DrawText(0, 1, $"{clock:hh}");
-                graphics.DrawText(0, 9, $"{clock:mm}");
-                graphics.DrawText(0, 17, $"{clock:ss}");
-                graphics.DrawText(0, 25, $"{clock:tt}");
-                graphics.Show();
-                Thread.Sleep(1000);
-            }
-        }
-
+        Start();
     }
+
+    void Initialize()
+    {
+        RgbPwmLed onboardLed = new RgbPwmLed(device: Device,
+            redPwmPin: Device.Pins.OnboardLedRed,
+            greenPwmPin: Device.Pins.OnboardLedGreen,
+            bluePwmPin: Device.Pins.OnboardLedBlue,
+            3.3f, 3.3f, 3.3f,
+            Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
+
+        onboardLed.StartPulse(Color.Red);
+
+        display = new Max7219(
+            device: Device, 
+            spiBus: Device.CreateSpiBus(), 
+            csPin: Device.Pins.D01, 
+            deviceCount: 4, 
+            maxMode: Max7219.Max7219Type.Display);
+        graphics = new GraphicsLibrary(display);
+        graphics.CurrentFont = new Font4x8();
+        graphics.Rotation = GraphicsLibrary.RotationType._180Degrees;
+
+        graphics.Clear();
+        graphics.DrawText(0, 1,  "WI");
+        graphics.DrawText(0, 9,  "FI");
+        graphics.DrawText(0, 17, "TI");
+        graphics.DrawText(0, 25, "ME");
+        graphics.Show();
+
+        pushButton = new PushButton(Device, Device.Pins.D04, ResistorMode.PullUp);
+        pushButton.Clicked += PushButtonClicked;
+
+        analogTemperature = new AnalogTemperature(
+            device: Device,
+            analogPin: Device.Pins.A00,
+            sensorType: AnalogTemperature.KnownSensorType.LM35
+        );            
+
+        Device.InitWiFiAdapter().Wait();
+
+        onboardLed.StartPulse(Color.Blue);
+
+        var result = Device.WiFiAdapter.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD);
+        if (result.ConnectionStatus != ConnectionStatus.Success)
+        {
+            onboardLed.StartPulse(Color.Magenta);
+            throw new Exception($"Cannot connect to network: {result.ConnectionStatus}");
+        }
+
+        onboardLed.StartPulse(Color.Green);
+    }
+
+    void PushButtonClicked(object sender, EventArgs e)
+    {
+        showDate = true;
+        Thread.Sleep(5000);
+        showDate = false;
+    }
+
+    async Task Start() 
+    {
+        var dateTime = await WeatherService.GetTimeAsync();            
+
+        Device.SetClock(new DateTime(
+            year: dateTime.Year, 
+            month: dateTime.Month, 
+            day: dateTime.Day, 
+            hour: dateTime.Hour, 
+            minute: dateTime.Minute, 
+            second: dateTime.Second));
+
+        while (true)
+        {
+            DateTime clock = DateTime.Now;
+            graphics.Clear();
+            graphics.DrawText(0, 1, $"{clock:hh}");
+            graphics.DrawText(0, 9, $"{clock:mm}");
+            graphics.DrawText(0, 17, $"{clock:ss}");
+            graphics.DrawText(0, 25, $"{clock:tt}");
+
+            if (showDate)
+            {
+                graphics.Clear();
+
+                graphics.DrawText(0, 1, $"{clock:dd}");
+                graphics.DrawText(0, 9, $"{clock:MM}");
+                graphics.DrawText(0, 17, $"{clock:yy}");
+                    
+                for (int i = 0; i < 8; i++)
+                {
+                    graphics.DrawPixel(i, 24);
+                }
+
+                float temperature = analogTemperature.Read().Result.Temperature.Value;
+
+                graphics.DrawText(0, 26, $"{(int) temperature}");
+            }
+                
+            graphics.Show();
+            Thread.Sleep(1000);
+        }
+    }
+}
 }
--- source/Hackster/WifiClock/Services/ClockService.cs
+++ source/Hackster/WifiClock/Services/ClockService.cs
@@ -1,53 +1,51 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using WifiClock.Models;
 
 namespace WifiClock.Services
 {
-    public static class WeatherService
+public static class WeatherService
+{
+    static string clockDataUri = "http://worldtimeapi.org/api/timezone/America/Vancouver/";        
+
+    static WeatherService() { }
+
+    public static async Task<DateTimeOffset> GetTimeAsync()
     {
-        static string clockDataUri = "http://worldtimeapi.org/api/timezone/America/Vancouver/";        
-
-        static WeatherService() { }
-
-        public static async Task<DateTimeOffset> GetTimeAsync()
+        using (HttpClient client = new HttpClient())
         {
-            using (HttpClient client = new HttpClient())
+            client.Timeout = new TimeSpan(0, 5, 0);
+
+            HttpResponseMessage response = await client.GetAsync($"{clockDataUri}");
+
+            try
             {
-                client.Timeout = new TimeSpan(0, 5, 0);
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
 
-                Console.WriteLine($"Sending request...");
+                response.EnsureSuccessStatusCode();
+                string json = await response.Content.ReadAsStringAsync();
+                var values = JsonSerializer.Deserialize(json, typeof(TimeEntity));
+                var reading = values as TimeEntity;
 
-                HttpResponseMessage response = await client.GetAsync($"{clockDataUri}");
+                stopwatch.Stop();
 
-                try
-                {
-                    Console.WriteLine($"Back, Serializing...");
-
-                    response.EnsureSuccessStatusCode();
-                    string json = await response.Content.ReadAsStringAsync();
-                    var values = System.Text.Json.JsonSerializer.Deserialize(json, typeof(TimeEntity));
-                    var reading = values as TimeEntity;
-
-                    Console.WriteLine($"Time: {reading.Datetime.ToString("hh:mm:ss")}");
-                    Console.WriteLine($"Time: {reading.Datetime.ToString("dd:MM:yyyy")}");
-
-
-
-                    return reading.Datetime;
-                }
-                catch (TaskCanceledException)
-                {
-                    Console.WriteLine("Request timed out.");
-                    return DateTimeOffset.MinValue;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Request went sideways: {e.Message}");
-                    return DateTimeOffset.MinValue;
-                }
+                return reading.Datetime.Add(stopwatch.Elapsed);
+            }
+            catch (TaskCanceledException)
+            {
+                Console.WriteLine("Request timed out.");
+                return DateTimeOffset.MinValue;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Request went sideways: {e.Message}");
+                return DateTimeOffset.MinValue;
             }
         }
     }
+}
 }
