15bf7d6fe767708d929e93eaf738d72eda3d0248
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-04-16 00:46:50 -0700

    Add ConnectedServo.Meadow project

---
+++ source/Hackster/MapleProjects/ConnectedServo.Meadow/Controllers/ServoController.cs
@@ -0,0 +1,86 @@
+﻿using Meadow.Foundation.Servos;
+using Meadow.Hardware;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConnectedServo.Meadow.Controllers
+{
+    public class ServoController
+    {
+        Servo servo;
+        Task animationTask = null;
+        CancellationTokenSource cancellationTokenSource = null;
+
+        protected int _rotationAngle;
+
+        protected bool initialized = false;
+
+        public static ServoController Current { get; private set; }
+
+        private ServoController() { }
+
+        static ServoController()
+        {
+            Current = new ServoController();
+        }
+
+        public void Initialize(IIODevice device, IPin PwmPin)
+        {
+            if (initialized) { return; }
+
+            Console.WriteLine("Initialize hardware...");
+            servo = new Servo(device, PwmPin, NamedServoConfigs.SG90);
+            servo.RotateTo(0);
+
+            initialized = true;
+
+            Console.WriteLine("Initialization complete.");
+        }
+
+        public void RotateTo(int angle) 
+        {
+            servo.RotateTo(angle);
+        }
+
+        public void StopSweep()
+        {
+            cancellationTokenSource?.Cancel();
+        }
+
+        public void StartSweep()
+        {
+            animationTask = new Task(async () =>
+            {
+                cancellationTokenSource = new CancellationTokenSource();
+                await StartSweep(cancellationTokenSource.Token);
+            });
+            animationTask.Start();
+        }
+        protected async Task StartSweep(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested) { break; }
+
+                while (_rotationAngle < 180)
+                {
+                    if (cancellationToken.IsCancellationRequested) { break; }
+
+                    _rotationAngle++;
+                    servo.RotateTo(_rotationAngle);
+                    await Task.Delay(15);
+                }
+
+                while (_rotationAngle > 0)
+                {
+                    if (cancellationToken.IsCancellationRequested) { break; }
+
+                    _rotationAngle--;
+                    servo.RotateTo(_rotationAngle);
+                    await Task.Delay(15);
+                }
+            }
+        }
+    }
+}
---
+++ source/Hackster/MapleProjects/ConnectedServo.Meadow/MapleRequestHandlers/ServoControllerRequestHandler.cs
@@ -0,0 +1,46 @@
+﻿using ConnectedServo.Meadow.Controllers;
+using Meadow.Foundation.Web.Maple.Server;
+using Meadow.Foundation.Web.Maple.Server.Routing;
+using System;
+
+namespace ConnectedServo.Meadow.MapleRequestHandlers
+{
+    public class ServoControllerRequestHandler : RequestHandlerBase
+    {
+        public ServoControllerRequestHandler() { }
+
+        [HttpPost]
+        public void RotateTo()
+        {
+            Console.WriteLine("POST: TurnOn!");
+            ServoController.Current.RotateTo(20);
+            StatusResponse();
+        }
+
+        [HttpPost]
+        public void StartSweep()
+        {
+            Console.WriteLine("POST: TurnOff!");
+            ServoController.Current.StartSweep();
+            StatusResponse();
+        }
+
+        [HttpPost]
+        public void StopSweep()
+        {
+            Console.WriteLine("POST: StartBlink!");
+            ServoController.Current.StopSweep();
+            StatusResponse();
+        }
+
+        [HttpPost]
+        void StatusResponse()
+        {
+            Console.WriteLine("POST");
+
+            Context.Response.ContentType = ContentTypes.Application_Text;
+            Context.Response.StatusCode = 200;
+            Send();
+        }
+    }
+}
