545c68dd3f0d4f37ff1df18bf11e29a7e4cdbecd
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-05-05 00:33:31 -0700

    Add BleRover.Client project

---
+++ source/Hackster/BluetoothProjects/BleRover.Client/BleRover.Client/View/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using BleRover.Client.ViewModel;
+using Xamarin.Forms;
+
+namespace BleRover.Client
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ source/Hackster/BluetoothProjects/BleRover.Client/BleRover.Client/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BleRover.Client.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
