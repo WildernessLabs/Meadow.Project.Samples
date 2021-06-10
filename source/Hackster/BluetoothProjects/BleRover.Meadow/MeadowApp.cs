122185d5cfeb66f99e744e22dc1638bab0e2be2a
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-06-10 00:35:42 -0700

    Add Bluetooth support to BleRover.Meadow project

--- source/Hackster/BluetoothProjects/BleRover.Meadow/MeadowApp.cs
+++ source/Hackster/BluetoothProjects/BleRover.Meadow/MeadowApp.cs
@@ -2,22 +2,34 @@
 using Meadow.Devices;
 using Meadow.Foundation.Leds;
 using Meadow.Foundation.Motors;
+using Meadow.Gateways.Bluetooth;
+using System;
 using System.Threading;
 
 namespace BleRover.Meadow
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
+        RgbLed led;
         PwmLed up, down, left, right;
         CarController carController;
 
         public MeadowApp()
         {
-            var led = new RgbLed(
-                Device,
-                Device.Pins.OnboardLedRed,
-                Device.Pins.OnboardLedGreen,
-                Device.Pins.OnboardLedBlue);
+            Initialize();
+
+            InitializeBluetooth();
+
+            //TestCar();
+        }
+
+        void Initialize() 
+        {
+            led = new RgbLed(
+                    Device,
+                    Device.Pins.OnboardLedRed,
+                    Device.Pins.OnboardLedGreen,
+                    Device.Pins.OnboardLedBlue);
             led.SetColor(RgbLed.Colors.Red);
 
             up = new PwmLed(Device, Device.Pins.D13, TypicalForwardVoltage.Red);
@@ -41,9 +53,85 @@ namespace BleRover.Meadow
 
             carController = new CarController(motorLeft, motorRight);
 
-            led.SetColor(RgbLed.Colors.Green);
+            led.SetColor(RgbLed.Colors.Yellow);
+        }
 
-            TestCar();
+        void InitializeBluetooth() 
+        {
+            Definition bleTreeDefinition;
+            CharacteristicBool onOffCharacteristic;
+
+            led.SetColor(RgbLed.Colors.Blue);
+
+            Device.InitCoprocessor();
+
+            onOffCharacteristic = new CharacteristicBool(
+                "On_Off",
+                Guid.NewGuid().ToString(),
+                CharacteristicPermission.Read | CharacteristicPermission.Write,
+                CharacteristicProperty.Read | CharacteristicProperty.Write);
+
+            bleTreeDefinition = new Definition(
+                "Meadow Rover",
+                new Service(
+                    "Service",
+                    253,
+                    onOffCharacteristic,
+                    new CharacteristicBool(
+                        "Up",
+                            uuid: "017e99d6-8a61-11eb-8dcd-0242ac1300aa",
+                            permissions: CharacteristicPermission.Write | CharacteristicPermission.Read,
+                            properties: CharacteristicProperty.Write | CharacteristicProperty.Read
+                        ),
+                    new CharacteristicBool(
+                        "Down",
+                            uuid: "017e99d6-8a61-11eb-8dcd-0242ac1300bb",
+                            permissions: CharacteristicPermission.Write | CharacteristicPermission.Read,
+                            properties: CharacteristicProperty.Write | CharacteristicProperty.Read
+                        ),
+                    new CharacteristicBool(
+                        "Left",
+                            uuid: "017e99d6-8a61-11eb-8dcd-0242ac1300cc",
+                            permissions: CharacteristicPermission.Write | CharacteristicPermission.Read,
+                            properties: CharacteristicProperty.Write | CharacteristicProperty.Read
+                        ),
+                    new CharacteristicBool(
+                        "Right",
+                            uuid: "017e99d6-8a61-11eb-8dcd-0242ac1300dd",
+                            permissions: CharacteristicPermission.Write | CharacteristicPermission.Read,
+                            properties: CharacteristicProperty.Write | CharacteristicProperty.Read
+                        )
+                    )
+                );
+
+            Device.BluetoothAdapter.StartBluetoothServer(bleTreeDefinition);
+
+            foreach (var characteristic in bleTreeDefinition.Services[0].Characteristics)
+            {
+                characteristic.ValueSet += (c, d) => 
+                {
+                    switch (c.Name) 
+                    {
+                        case "Up": 
+                            up.IsOn = (bool)d;
+                            break;
+
+                        case "Down":
+                            down.IsOn = (bool)d;
+                            break;
+
+                        case "Left":
+                            left.IsOn = (bool)d;
+                            break;
+
+                        case "Right":
+                            right.IsOn = (bool)d;
+                            break;
+                    }             
+                };
+            }
+
+            led.SetColor(RgbLed.Colors.Green);
         }
 
         void TestCar()
