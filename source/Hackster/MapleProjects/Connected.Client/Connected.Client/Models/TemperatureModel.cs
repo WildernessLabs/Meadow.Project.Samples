70d3399285f991d4110bcfb26635bc2f0110a015
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-08-20 01:11:48 -0700

    Update Connected.Client app

---
+++ source/Hackster/MapleProjects/Connected.Client/Connected.Client/Models/TemperatureModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Connected.Client.Model
+{
+    public class TemperatureModel
+    {
+        public string DateTime { get; set; }
+        public string Temperature { get; set; }
+    }
+}
