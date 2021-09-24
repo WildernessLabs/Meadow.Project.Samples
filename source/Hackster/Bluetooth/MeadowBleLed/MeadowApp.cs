5ffd33b481190f526446f0518a1554ac071f9284
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-09-24 00:11:57 -0700

    Update/CleanUp to MeadowBleLed

--- source/Hackster/Bluetooth/MeadowBleLed/MeadowApp.cs
+++ source/Hackster/Bluetooth/MeadowBleLed/MeadowApp.cs
@@ -8,14 +8,16 @@ namespace MeadowBleLed
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
+        Color selectedColor;
+
         RgbPwmLed onboardLed;
 
         Definition bleTreeDefinition;
         CharacteristicBool isOnCharacteristic;
         CharacteristicInt32 colorCharacteristic;
 
-        readonly string IsOnUUID = "24517ccc888e4ffc9da521884353b08d";
-        readonly string ColorUUID = "5a0bb01669ab4a49a2f2de5b292458f3";
+        readonly string IS_ON = "24517ccc888e4ffc9da521884353b08d";
+        readonly string COLOR = "5a0bb01669ab4a49a2f2de5b292458f3";
 
         public MeadowApp()
         {
@@ -36,22 +38,24 @@ namespace MeadowBleLed
 
             bleTreeDefinition = GetDefinition();
             Device.BluetoothAdapter.StartBluetoothServer(bleTreeDefinition);
-
-            isOnCharacteristic.ValueSet += IsOnCharacteristicValueSet;
+            
             colorCharacteristic.ValueSet += ColorCharacteristicValueSet;
+            isOnCharacteristic.ValueSet += IsOnCharacteristicValueSet;
         }
 
         void IsOnCharacteristicValueSet(ICharacteristic c, object data)
         {
-            if ((int)data > 0)
+            if ((bool)data)
             {
-                onboardLed.IsOn = false;
-                isOnCharacteristic.SetValue(true);
+                PulseColor(selectedColor);
+                onboardLed.IsOn = true;
+                isOnCharacteristic.SetValue(false);
             }
             else
             {
-                onboardLed.IsOn = true;
-                isOnCharacteristic.SetValue(false);
+                onboardLed.Stop();
+                onboardLed.IsOn = false;
+                isOnCharacteristic.SetValue(true);
             }
         }
 
@@ -59,18 +63,18 @@ namespace MeadowBleLed
         {
             int color = (int)data;
 
-            byte r = (byte)(color >> 16);
-            byte g = (byte)(color >> 8);
-            byte b = (byte)(color);
+            byte r = (byte)((color >> 16) & 0xff);
+            byte g = (byte)((color >> 8) & 0xff);
+            byte b = (byte)((color >> 0) & 0xff);
 
-            var newColor = new Color(r / 255.0, g / 255.0, b / 255.0);
-            PulseColor(newColor);
+            PulseColor(new Color(r / 255.0, g / 255.0, b / 255.0));
 
             colorCharacteristic.SetValue(color);
         }
 
         void PulseColor(Color color)
         {
+            selectedColor = color;
             onboardLed.Stop();
             onboardLed.StartPulse(color);
         }
@@ -79,23 +83,21 @@ namespace MeadowBleLed
         {
             isOnCharacteristic = new CharacteristicBool(
                 name: "On_Off",
-                uuid: IsOnUUID,
+                uuid: IS_ON,
                 permissions: CharacteristicPermission.Read | CharacteristicPermission.Write,
                 properties: CharacteristicProperty.Read | CharacteristicProperty.Write);
 
             colorCharacteristic = new CharacteristicInt32(
                 name: "CurrentColor",
-                uuid: ColorUUID,
+                uuid: COLOR,
                 permissions: CharacteristicPermission.Read | CharacteristicPermission.Write,
                 properties: CharacteristicProperty.Read | CharacteristicProperty.Write);
 
-            colorCharacteristic.SetValue(0x0000FF00); //blue
-
             var service = new Service(
-                 "ServiceA",
-                 253,
-                 isOnCharacteristic,
-                 colorCharacteristic
+                name: "ServiceA",
+                uuid: 253,
+                isOnCharacteristic, 
+                colorCharacteristic
             );
 
             return new Definition("MeadowRGB", service);
