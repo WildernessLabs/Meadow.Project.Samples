072acef4973b2a4bbd66ae218ce9618de0bf83c5
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-12-10 19:07:39 -0800

    [MorseCode] WIP

--- source/Hackster/MorseCodeTrainer/MeadowApp.cs
+++ source/Hackster/MorseCodeTrainer/MeadowApp.cs
@@ -7,20 +7,25 @@ using Meadow.Foundation.Leds;
 using Meadow.Foundation.Sensors.Buttons;
 using Meadow.Hardware;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Timers;
 
 namespace MorseCodeTrainer
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        RgbPwmLed onboardLed;
+        Dictionary<string, string> morseCode;
 
-        PushButton button;
-        Stopwatch stopWatch;
+        RgbPwmLed onboardLed;
+        PushButton btnTyper;
+        PushButton btnFunction;
         GraphicsLibrary graphics;
 
-        System.Timers.Timer timer;
+        Timer timer;
+        Stopwatch stopWatch;
         string character;
+        string text;
 
         public MeadowApp()
         {
@@ -29,8 +34,6 @@ namespace MorseCodeTrainer
 
         void Initialize()
         {
-            Console.WriteLine("Initialize hardware...");
-
             onboardLed = new RgbPwmLed(device: Device,
                 redPwmPin: Device.Pins.OnboardLedRed,
                 greenPwmPin: Device.Pins.OnboardLedGreen,
@@ -56,24 +59,85 @@ namespace MorseCodeTrainer
             };
             graphics.Clear();
             graphics.DrawRectangle(0, 0, 240, 240);
-            graphics.DrawText(5, 5, "Morse Code Trainer");
+            graphics.DrawText(24, 15, "Morse Code Coach");
+            graphics.DrawHorizontalLine(24, 41, 196, Color.Red);
+            graphics.DrawText(102, 70, "A", Color.White, GraphicsLibrary.ScaleFactor.X3);
+            //graphics.DrawText(50, 100, "-----", GraphicsLibrary.ScaleFactor.X3);
             graphics.Show();
 
-            button = new PushButton(device: Device, Device.Pins.D02);
-            button.PressStarted += ButtonPressStarted;
-            button.PressEnded += ButtonPressEnded;
+            btnTyper = new PushButton(device: Device, Device.Pins.D02);
+            btnTyper.PressStarted += ButtonPressStarted;
+            btnTyper.PressEnded += ButtonPressEnded;
+
+            btnFunction = new PushButton(device: Device, Device.Pins.D03);
+            btnFunction.Clicked += BtnFunctionClicked;
 
             stopWatch = new Stopwatch();
 
-            timer = new System.Timers.Timer(3000);
+            timer = new Timer(2000);
             timer.Elapsed += TimerElapsed;
+
+            LoadMorseCode();
 
             onboardLed.SetColor(Color.Green);
         }
 
-        private void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        private void BtnFunctionClicked(object sender, EventArgs e)
         {
-            Console.WriteLine(character);
+            text = text.Substring(0, text.Length - 1);
+            Console.WriteLine(text);
+        }
+
+        void LoadMorseCode() 
+        {
+            morseCode = new Dictionary<string, string>();            
+            morseCode.Add("O-"   , "A");
+            morseCode.Add("-OOO" , "B");
+            morseCode.Add("-O-O" , "C");
+            morseCode.Add("-OO"  , "D");
+            morseCode.Add("O"    , "E");
+            morseCode.Add( "OO-O", "F");
+            morseCode.Add("--O"  , "G");
+            morseCode.Add("OOOO" , "H");
+            morseCode.Add("OO"   , "I");
+            morseCode.Add("O---" , "J");
+            morseCode.Add("-O-"  , "K");
+            morseCode.Add("O-OO" , "L");
+            morseCode.Add("--"   , "M");
+            morseCode.Add("-O"   , "N");
+            morseCode.Add("---"  , "O");
+            morseCode.Add("O--O" , "P");
+            morseCode.Add("--O-" , "Q");
+            morseCode.Add("O-O"  , "R");
+            morseCode.Add("OOO"  , "S");
+            morseCode.Add("-"    , "T");
+            morseCode.Add("OO-"  , "U");
+            morseCode.Add("OOO-" , "V");
+            morseCode.Add("O--"  , "W");
+            morseCode.Add("-OO-" , "X");
+            morseCode.Add("-O--" , "Y");
+            morseCode.Add("--OO" , "Z");
+            morseCode.Add("-----", "0");
+            morseCode.Add("O----", "1");
+            morseCode.Add("OO---", "2");
+            morseCode.Add("OOO--", "3");
+            morseCode.Add("OOOO-", "4");
+            morseCode.Add("OOOOO", "5");
+            morseCode.Add("-OOOO", "6");
+            morseCode.Add("--OOO", "7");
+            morseCode.Add("---OO", "8");
+            morseCode.Add("----O", "9");
+        }
+
+        void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (!morseCode.ContainsKey(character))
+            {
+                return;
+            }
+
+            text += morseCode[character];
+            Console.WriteLine(text);
             character = string.Empty;
             timer.Stop();
         }
@@ -100,45 +164,5 @@ namespace MorseCodeTrainer
 
             timer.Start();
         }
-    }
-
-    public class MorseCodeConstants 
-    {
-        public const string A = "O-";
-        public const string B = "-OOO";
-        public const string C = "-O-O";
-        public const string D = "-OO";
-        public const string E = "O";
-        public const string F = "OO-O";
-        public const string G = "--O";
-        public const string H = "OOOO";
-        public const string I = "OO";
-        public const string J = "O---";
-        public const string K = "-O-";
-        public const string L = "O-OO";
-        public const string M = "--";
-        public const string N = "-O";
-        public const string O = "---";
-        public const string P = "O--O";
-        public const string Q = "--O-";
-        public const string R = "O-O";
-        public const string S = "OOO";
-        public const string T = "-";
-        public const string U = "OO-";
-        public const string V = "OOO-";
-        public const string W = "O--";
-        public const string X = "-OO-";
-        public const string Y = "-O--";
-        public const string Z = "--OO";
-        public const string _0 = "-----";
-        public const string _1 = "O----";
-        public const string _2 = "OO---";
-        public const string _3 = "OOO--";
-        public const string _4 = "OOOO-";
-        public const string _5 = "OOOOO";
-        public const string _6 = "-OOOO";
-        public const string _7 = "--OOO";
-        public const string _8 = "---OO";
-        public const string _9 = "----O";
     }
 }
