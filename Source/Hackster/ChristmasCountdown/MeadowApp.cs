927fc08b2709b30522dd7ea7fabfc6a9bb6241c5
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-10 15:29:17 -0800

    [ChrismasCountdown] Fix meadow version

--- Source/Hackster/ChristmasCountdown/MeadowApp.cs
+++ Source/Hackster/ChristmasCountdown/MeadowApp.cs
@@ -11,7 +11,7 @@ using Meadow.Gateway.WiFi;
 namespace ChristmasCountdown
 {
     // public class MeadowApp : App<F7Micro, MeadowApp> <- If you have a Meadow F7v1.*
-    public class MeadowApp : App<F7Micro, MeadowApp>
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
     {        
         CharacterDisplay display;
 
