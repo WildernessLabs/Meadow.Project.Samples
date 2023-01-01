836235dd1f90620a23f541458f44c4757af3ecc1
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-01-01 01:02:36 -0800

    Update Maple Projects

--- Source/Hackster/Maple/MeadowMapleServo/MeadowApp.cs
+++ Source/Hackster/Maple/MeadowMapleServo/MeadowApp.cs
@@ -2,25 +2,25 @@
 using Meadow.Devices;
 using Meadow.Foundation;
 using Meadow.Foundation.Leds;
+using Meadow.Foundation.Servos;
 using Meadow.Foundation.Web.Maple;
-using Meadow.Gateway.WiFi;
 using Meadow.Hardware;
+using Meadow.Units;
 using MeadowMapleServo.Controllers;
 using System;
 using System.Threading.Tasks;
-using Meadow.Units;
-using Meadow.Foundation.Servos;
 
 namespace MeadowMapleServo
 {
     // public class MeadowApp : App<F7FeatherV1> <- If you have a Meadow F7v1.*
     public class MeadowApp : App<F7FeatherV2>
     {
+        RgbPwmLed onboardLed;
         MapleServer mapleServer;
 
         public override async Task Initialize()
         {
-            var onboardLed = new RgbPwmLed(device: Device,
+            onboardLed = new RgbPwmLed(device: Device,
                 redPwmPin: Device.Pins.OnboardLedRed,
                 greenPwmPin: Device.Pins.OnboardLedGreen,
                 bluePwmPin: Device.Pins.OnboardLedBlue);
@@ -29,10 +29,14 @@ namespace MeadowMapleServo
             ServoController.Instance.RotateTo(new Angle(NamedServoConfigs.SG90.MinimumAngle));
 
             var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
+            wifi.NetworkConnected += NetworkConnected;
 
             await wifi.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD, TimeSpan.FromSeconds(45));
+        }
 
-            mapleServer = new MapleServer(wifi.IpAddress, 5417, true, logger: Resolver.Log);
+        private void NetworkConnected(INetworkAdapter sender, NetworkConnectionEventArgs args)
+        {
+            mapleServer = new MapleServer(sender.IpAddress, 5417, true, logger: Resolver.Log);
             mapleServer.Start();
 
             onboardLed.SetColor(Color.Green);
--- Source/Hackster/Maple/MeadowMapleTemperature/MeadowApp.cs
+++ Source/Hackster/Maple/MeadowMapleTemperature/MeadowApp.cs
@@ -2,7 +2,6 @@
 using Meadow.Devices;
 using Meadow.Foundation;
 using Meadow.Foundation.Web.Maple;
-using Meadow.Gateway.WiFi;
 using Meadow.Hardware;
 using MeadowMapleTemperature.Controllers;
 using System;
@@ -19,13 +18,17 @@ namespace MeadowMapleTemperature
         {
             LedController.Instance.SetColor(Color.Red);
 
-            var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
-
-            await wifi.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD, TimeSpan.FromSeconds(45));
-
             TemperatureController.Instance.Initialize();
 
-            mapleServer = new MapleServer(wifi.IpAddress, 5417, true, logger: Resolver.Log);
+            var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
+            wifi.NetworkConnected += NetworkConnected;
+
+            await wifi.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD, TimeSpan.FromSeconds(45));
+        }
+
+        private void NetworkConnected(INetworkAdapter sender, NetworkConnectionEventArgs args)
+        {
+            mapleServer = new MapleServer(sender.IpAddress, 5417, true, logger: Resolver.Log);
             mapleServer.Start();
 
             LedController.Instance.SetColor(Color.Green);
