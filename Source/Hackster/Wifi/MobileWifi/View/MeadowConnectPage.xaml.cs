0f32b9dd1e09763a0c7b4e883ffb486afe552d91
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-03-18 23:34:44 -0700

    [MobileWifi] Minor refactor

--- Source/Hackster/Wifi/MobileWifi/View/MeadowConnectPage.xaml.cs
+++ Source/Hackster/Wifi/MobileWifi/View/MeadowConnectPage.xaml.cs
@@ -13,17 +13,16 @@ namespace MobileWifi.View
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            (BindingContext as BaseViewModel).CmdSearchForDevices.Execute(null);
+
+            if (!(BindingContext as BaseViewModel).IsConnected)
+            {
+                (BindingContext as BaseViewModel).CmdSearchForDevices.Execute(null);
+            }
         }
 
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
-
-            if ((BindingContext as BaseViewModel).IsConnected)
-            {
-                (BindingContext as BaseViewModel).CmdToggleConnection.Execute(null);
-            }
         }
     }
 }
